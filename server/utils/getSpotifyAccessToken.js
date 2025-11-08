c9db8592b7913e0c4d619dcf943d063c7b8d2c99
KinshukSS2 <kinshuk380@gmail.com>
2025-11-08 14:46:31 +0530

    Add Spotify credential debugging

--- server/utils/getSpotifyAccessToken.js
+++ server/utils/getSpotifyAccessToken.js
@@ -14,6 +14,11 @@ export async function getSpotifyAccessToken() {
   const clientId = process.env.SPOTIFY_CLIENT_ID;
   const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
+  console.log('Spotify Client ID:', clientId);
+  console.log('Spotify Client Secret (first 10 chars):', clientSecret?.substring(0, 10));
+  console.log('Client ID length:', clientId?.length);
+  console.log('Client Secret length:', clientSecret?.length);
+
   const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
 
   const res = await fetch("https://accounts.spotify.com/api/token", {
